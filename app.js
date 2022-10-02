c4292b1fa1abb30c9c333c078515826f613bc46b
shubh-37 <shubh.arya37@gmail.com>
2022-10-02 11:45:10 +0530

    done with input and output function

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-var calculateChange = document.querySelector("#calculate-change");
+var btnCalculateChange = document.querySelector("#calculate-change");
 var inputBillAmount = document.querySelector("#bill-amount");
 var inputCashGiven = document.querySelector("#cash-given");
 
-function clickHandler(){
-    console.log("clicked");
+// function hideMessage(){
+
+// }
+
+function errorHandler(){
+
 }
 
-calculateChange.addEventListener("click", clickHandler);
+function clickHandler(){
+    var billAmt = inputBillAmount.value;
+    var cashGiven = inputCashGiven.value;
+    if(billAmt === NaN){
+        errorHandler("please enter a number to proceed further!")
+    }
+    if (billAmt > 0){
+        if(cashGiven > billAmt){
+            calculateChange(billAmt);
+        }else{
+            errorHandler("Do you want to wash dishes instead?")
+        }
+    }else{
+        errorHandler("The bill amount should be greater than 0")
+    }
+}
+
+btnCalculateChange.addEventListener("click", clickHandler);
